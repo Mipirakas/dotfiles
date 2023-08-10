add80e7c4dbf9d046c01822e6b3711b935278fa9
Boris Michiels <borismichiels@gmail.com>
2023-08-10 14:09:52 +0200

    update platform-tools.sh

--- scripts/platform-tools.sh
+++ scripts/platform-tools.sh
@@ -2,6 +2,6 @@
 
 wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
 unzip -o platform-tools-latest-linux.zip
-sudo cp platform-tools/adb /usr/bin/adb
-sudo cp platform-tools/fastboot /usr/bin/fastboot
+sudo cp platform-tools/adb /usr/local/bin/adb
+sudo cp platform-tools/fastboot /usr/local/bin/fastboot
 sudo rm -rf platform-tools-latest-linux.zip platform-tools
